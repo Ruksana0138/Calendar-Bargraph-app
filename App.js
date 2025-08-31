ce44fc299b1e102220e9f7b71f7f60cb64114ac3
Ruksana0138 <81854065+Ruksana0138@users.noreply.github.com>
2025-08-31 11:00:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CalendarView from "./components/Calendar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>📅 React Big Calendar with Bar Graph</h1>
+      <CalendarView />
+    </div>
+  );
+}
+
+export default App;
+
